aa51f58beafb66b9daccb8988bf11d1df39c9ec4
Chandan Prakash <abchandan11@gmail.com>
2020-06-30 11:45:17 +0530

    testing added

--- src/App.test.js
+++ src/App.test.js
@@ -1,14 +1,6 @@
+const sum = require('./sum');
 
-    import React from 'react';
-    import ReactDOM from 'react-dom';
-    import App from './App';
 
-    it('renders without crashing', () => {
-      const div = document.createElement('div');
-      ReactDOM.render(<App />, div);
-      ReactDOM.unmountComponentAtNode(div);
-    });
-    
     describe('Addition', () => {
         it('knows that 2 and 2 make 4', () => {
           expect(2 + 2).toBe(4);
@@ -17,8 +9,11 @@
 
     describe('correct hello world gets printed ', () => {
         it('message returned is Hello world ', () => {
-            const result = ""
-            console.log(result)
           expect(2+2).toBe(4);
         });
       });
+    
+      test('adds 1 + 2 to equal 3', () => {
+        expect(sum(1, 2)).toBe(3);
+      });
+    
